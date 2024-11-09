f636632b04b72279daf6223e256f4b9cdbcce94e
Abhay <abhaykvincent@gmail.com>
2024-11-09 22:40:32 +0530

    Projects code Refactor To reduce line complexity and enhance readability

--- src/features/Projects/Projects.jsx
+++ src/features/Projects/Projects.jsx
@@ -1,93 +1,100 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Import React Router components
+import { useDispatch, useSelector } from 'react-redux';
+// Selectors and actions
+import { selectProjects } from '../../app/slices/projectsSlice';
+import { selectUserStudio } from '../../app/slices/authSlice';
+import { openModal } from '../../app/slices/modalSlice';
+// Utility functions
+import { getRecentProjects } from '../../utils/projectFilters';
+// Components
 import AddProjectModal from '../../components/Modal/AddProject';
-import './Projects.scss';
 import ProjectCard from '../../components/Project/ProjectCard/ProjectCard';
 import Refresh from '../../components/Refresh/Refresh';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectProjects } from '../../app/slices/projectsSlice';
-import { openModal } from '../../app/slices/modalSlice';
-import { getRecentProjects } from '../../utils/projectFilters';
-import { selectUserStudio } from '../../app/slices/authSlice';
+// Styles
+import './Projects.scss';
 
 function Projects() {
-    const defaultStudio = useSelector(selectUserStudio)
-    const dispatch = useDispatch()
+    const defaultStudio = useSelector(selectUserStudio);
+    const dispatch = useDispatch();
     document.title = `${defaultStudio.name} | Projects`;
 
-    const projects = useSelector(selectProjects)
-    const [recentProjects, setRecentProjects] = useState([])
+    const projects = useSelector(selectProjects);
+    const [recentProjects, setRecentProjects] = useState([]);
+
     useEffect(() => {
-        setRecentProjects(getRecentProjects(projects))
+        setRecentProjects(getRecentProjects(projects));
     }, [projects]);
+
+    const handleNewProjectClick = () => dispatch(openModal('createProject'));
+
+    const renderProjectList = () => {
+        if (recentProjects.length === 0) {
+            return (
+                <>
+                    <div className="section recent">
+                        <h3 className="section-heading">Recent Projects</h3>
+                    </div>
+                    <div className="project new" onClick={handleNewProjectClick}>
+                        <div className="project-cover"></div>
+                        <div className="project-details">
+                            <div className="details-top">
+                                <h4 className="project-title">Create Your First Project</h4>
+                                <p className="project-type"></p>
+                            </div>
+                        </div>
+                        <div className="project-options"></div>
+                    </div>
+                </>
+            );
+        }
+
+        return recentProjects.map((project) => (
+            <ProjectCard project={project} key={project.id} />
+        ));
+    };
+
     return (
         <>
-          <AddProjectModal />
-
-        <main className="projects">
-            <div className="projects-header">
-                <h1>Projects</h1>
-                <div className="actions">
-                    <div className="button primary icon add"
-                        onClick={()=>dispatch(openModal('createProject'))}
-                    >New</div>
-                </div>
-            </div>
-            <div className="view-control">
-                <div className="control-wrap">
-                    <div className="controls">
-                        <div className={`control ctrl-all active`} >All</div>
-                        <div className={`control ctrl-draft`} >Archived</div>
+            <AddProjectModal />
+            <main className="projects">
+                <div className="projects-header">
+                    <h1>Projects</h1>
+                    <div className="actions">
+                        <div className="button primary icon add" onClick={handleNewProjectClick}>
+                            New
+                        </div>
                     </div>
-                    <div className={`active`}></div>
                 </div>
-                <div className="control-wrap">
-                    <div className="controls">
-                        <div className={`control ctrl-all active`} ><div className="icon card-view"></div></div>
-                        <div className={`control ctrl-active disabled`} ><div className="icon list-view"></div></div>
+
+                <div className="view-control">
+                    <div className="control-wrap">
+                        <div className="controls">
+                            <div className="control ctrl-all active">All</div>
+                            <div className="control ctrl-draft">Archived</div>
+                        </div>
+                        <div className="active"></div>
                     </div>
-                    <div className={`active`}></div>
+                    <div className="control-wrap">
+                        <div className="controls">
+                            <div className="control ctrl-all active">
+                                <div className="icon card-view"></div>
+                            </div>
+                            <div className="control ctrl-active disabled">
+                                <div className="icon list-view"></div>
+                            </div>
+                        </div>
+                        <div className="active"></div>
+                    </div>
                 </div>
-            </div>
-            <div className="projects-list">
-                { 
-                recentProjects.length !== 0? (
-                    recentProjects.map((project, index) => (
-                        <ProjectCard 
-                        project={project}
-                        key={project.id}
-                    /> 
-                    ))) : (
-                        <>
-                            <div className="section recent">
-                                <h3 className='section-heading'>Recent Projects</h3>
-                            </div>
 
-                            <div className="project new"
-                                onClick={()=>{dispatch(openModal('createProject'))}}
-                            >
-                                <div className="project-cover" ></div>
-                                <div className="project-details">
-                                    <div className="details-top">
+                <div className="projects-list">
+                    {renderProjectList()}
+                </div>
 
-                                        <h4 className="project-title">Create Your First Project</h4>
-                                        <p className="project-type"></p>
-                                    </div>
-                                </div>
-                                <div className="project-options"></div>
-                            </div>
-                        </>
-                    )
-                }
-            </div>
-
-            {/* Refresh Projects Data from cloud */}
-            <Refresh/>
-            
-        </main>
+                <Refresh />
+            </main>
         </>
     );
 }
 
 export default Projects;
-// Line Complexity  1.0 -> 
