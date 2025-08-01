73e58931b5d089d0f5a46e7dd593bdc11fce2571
Abhay <abhaykvincent@gmail.com>
2025-08-01 14:40:27 +0530

    restructure share after qr implementation

--- src/components/Modal/ShareGallery.jsx
+++ src/components/Modal/ShareGallery.jsx
@@ -130,14 +130,34 @@ function ShareGallery({project }) {
             <div className="control minimize"></div>
             <div className="control maximize"></div>
           </div>
-          <div className="modal-title">Share Galleries</div>
+          <div className="modal-title">Share Gallery</div>
         </div>
         <div className='modal-body'>
           <div className="form-section">
             {/* map project collections and render it with a check box to select galleries to share */}
-            <div className="gallery-details">
-              <h4>{project?.name}</h4>
-              
+            <div className="share-project-details">
+              <div className="project-cover">
+                <img src={project?.projectCover || '/images/default-cover.jpg'} alt="Project Cover" />
+              </div>
+              <div className="gallery-details">
+                <div className="gallery-details-project">
+                 <h4>{project?.name}</h4>
+                  <div className="project-type">{project?.type}</div>
+
+                </div>
+                <div className="link-pin">
+                  <div className='link' >
+
+                    <div className="link-container">
+                      <a className='linkToGallery' href={getGalleryURL('share',domain,project?.id)} target='_blank' > ...{getGalleryURL('share',domain,project?.id).slice(-40)}
+                        <div className="button icon icon-only open-in-new"></div>
+                      </a>
+                    </div>
+                  </div>
+                    <div className="button primary outline text-only  icon copy"></div>
+
+                </div>
+              </div>
             </div>
 
               <div className="select-galleries">
@@ -209,18 +229,7 @@ function ShareGallery({project }) {
 
               </div>
 
-              {/* <p className="client-label">Gallery link</p>
-              <div className="link-pin">
-                <div className='link' >
-
-                  <div className="link-container">
-                    <a className='linkToGallery' href={getGalleryURL('share',domain,project?.id)} target='_blank' >{getGalleryURL('share',domain,project?.id)}
-                      <div className="button icon icon-only open-in-new"></div>
-                    </a>
-                  </div>
-
-                </div>
-              </div> */}
+              
 
               <div className="gallery-view-status">
                 <div className="link-group">
@@ -297,7 +306,7 @@ function ShareGallery({project }) {
             // go to https://wa.me/[phone number]?text=[pre-filled message]
             window.open(`https://wa.me/?text=Hey, I have a project that I'd like to share with you. Check it out at ${getGalleryURL('share',domain,project.id)}/`,'_blank');
           }}
-          >Share</div>
+          >Done</div>
         </div>
       </div>
       <div className="modal-backdrop" onClick={onClose}></div>
