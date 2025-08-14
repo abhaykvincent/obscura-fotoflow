ad7f82ea4b43d3334f74bf673649466ad772d482
Abhay <abhaykvincent@gmail.com>
2025-08-14 12:38:20 +0530

    🐛 fix: Use the section-wrapper itself as the drag handle, like the drag-handle component.

--- src/components/ImageGalleryDesigner/GallerySections/GallerySections.jsx
+++ src/components/ImageGalleryDesigner/GallerySections/GallerySections.jsx
@@ -50,14 +50,14 @@ const SortableSection = ({ section, index, id, collectionId, collectionName, han
   const SectionComponent = sectionComponents[section.type];
 
   return (
-    <div ref={setNodeRef} style={style} className="section-wrapper">
+    <div ref={setNodeRef} style={style} className="section-wrapper" {...attributes} {...listeners}>
       <div className="toolbar vertical">
         <div className="tools-container">
           <button className='button text-only icon section-settings dark-icon'></button>
           <button className='button text-only icon delete-red dark-icon'></button>
         </div>
-        <div className="tools-container drag-handle" {...attributes} {...listeners}>
-          <BsGripVertical />
+        <div className="tools-container">
+          {/* No specific drag handle icon needed here anymore */}
         </div>
       </div>
       <div className="add-section-icon-container top">
