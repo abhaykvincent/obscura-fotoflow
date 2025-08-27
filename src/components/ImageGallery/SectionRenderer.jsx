86fd922318a8cf1d31a119172e00ca3f34c42a43
Abhay <abhaykvincent@gmail.com>
2025-08-27 15:11:55 +0530

    :bug: fix: Render all section types in SectionRenderer and apply isViewOnly prop

--- src/components/ImageGallery/SectionRenderer.jsx
+++ src/components/ImageGallery/SectionRenderer.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ImageGrid from '../ImageGalleryDesigner/GallerySections/ImageGrid';
+import Carousel from '../ImageGalleryDesigner/GallerySections/Carousel';
+import TextBlock from '../ImageGalleryDesigner/GallerySections/TextBlock';
+import SubGallery from '../ImageGalleryDesigner/GallerySections/SubGallery';
+import Embed from '../ImageGalleryDesigner/GallerySections/Embed';
 
 const SectionRenderer = ({ section }) => {
   switch (section.type) {
     case 'image-grid':
       return <ImageGrid section={section} isViewOnly={true} />;
+    case 'carousel':
+      return <Carousel section={section} isViewOnly={true} />;
+    case 'text-block':
+      return <TextBlock section={section} isViewOnly={true} />;
+    case 'sub-gallery':
+      return <SubGallery section={section} isViewOnly={true} />;
+    case 'embed':
+      return <Embed section={section} isViewOnly={true} />;
     default:
       return <div>Unknown Section Type</div>;
   }
